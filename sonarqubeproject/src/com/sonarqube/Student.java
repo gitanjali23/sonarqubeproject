5dcfc76f93ff689b6eb5271c8898b2d1a8d23ed2
Pallavi <Pallavi@DESKTOP-GLNSG25>
2019-07-13 18:39:41 +0530

    sonarqube is use to analyse source code

---
+++ sonarqubeproject/src/com/sonarqube/Student.java
@@ -0,0 +1,10 @@
+package com.sonarqube;
+
+public class Student {
+	static int id=10;
+	static String name="pallavi";
+	public static void main(String[] args) { 
+		System.out.println(id);
+		System.out.println(name);
+	}
+}
